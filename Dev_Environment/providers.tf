d1ea26758b5afebed6770a9d8d9eaf17d0a17cfd
yogeshraj28 <34445437+yogeshraj-au@users.noreply.github.com>
2023-09-22 10:58:29 +0530

    Update Provider

--- Dev_Environment/providers.tf
+++ Dev_Environment/providers.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "default"
 }
